cccc68f491930c1369ee43158bcba5bba55d184e
Natalia Dokuchaeva <natdok85@gmail.com>
2019-05-22 20:41:27 +0300

    second commit variables

--- variables/main.js
+++ variables/main.js
@@ -4,3 +4,7 @@ let physics = 4;
 
 let result = (algebra + geography + physics) / 3;
 console.log(result);
+
+let name = "Наталья";
+let message = `Привет, мир! Меня зовут ${name}`;
+console.log(message);
